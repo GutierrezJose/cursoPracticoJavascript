d93d67815b1b4b81106577b2f1bd59597a8f9f1e
JoseGutierrez <gutierreztorrezjose@gmail.com>
2022-08-01 17:08:36 -0400

    se termino el taller 4

---
+++ salarios.js
@@ -0,0 +1,73 @@
+const colombia = [];
+colombia.push({
+    name:"Sandra",
+    salary: 700,
+});
+colombia.push({
+    name: "Carla",
+    salary: 600,
+});
+colombia.push({
+    name: "Blanca",
+    salary: 200
+});
+colombia.push({
+    name: "Mariela",
+    salary: 1500
+});
+colombia.push({
+    name: "Marcela",
+    salary: 2000
+})
+colombia.push({
+    name: "Fernanda",
+    salary: 2600
+});
+colombia.push({
+    name: "Carolina",
+    salary: 900
+});
+colombia.push({
+    name: "Raquel",
+    salary: 1500,
+});
+colombia.push({
+    name: "Roberta",
+    salary: 630
+});
+colombia.push({
+    name: "Maria",
+    salary: 3000
+});
+colombia.push({
+    name: "Carlos",
+    salary: 2650
+});
+colombia.push({
+    name: "Jose",
+    salary: 5000
+});
+colombia.push({
+    name: "Oscar",
+    salary: 7000
+});
+colombia.push({
+    name: "Franz",
+    salary: 2700
+});
+colombia.push({
+    name: "Evo",
+    salary: 200000
+});
+colombia.push({
+    name: "Alvaro",
+    salary: 3000
+});
+colombia.push({
+    name: "Roxana",
+    salary: 1000,
+})
+colombia.push({
+    name: "Carol",
+    salary: 4000
+});
